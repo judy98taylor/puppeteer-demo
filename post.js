ba004ce748589939f01d70becaf9df59c2668fc2
Rena <zd9027@gmail.com>
2018-04-08 15:44:40 +0800

    update

--- post.js
+++ post.js
@@ -6,16 +6,22 @@ let sourceProjectName = process.cwd().split("/")[
 ];
 let commonPath = process.cwd().replace(sourceProjectName, "");
 let destProjectName = "asd";
+console.log(commonPath)
 
 // let sourceDistPath = "puppeteer-demo";
 // let destDistPath = "asd";
+console.log(process.cwd())
 
 if (!which("git")) {
   echo("Sorry, this script requires git");
   exit(1);
 }
-let who = exec("git config user.name", { silent: true }).stdout;
-let sourceRemoteUrl = exec("git config remote.origin.url", { silent: true })
+let who = exec("git config user.name", {
+  silent: true
+}).stdout;
+let sourceRemoteUrl = exec("git config remote.origin.url", {
+    silent: true
+  })
   .stdout;
 
 // build完成之后 应该只更新dist
@@ -32,49 +38,60 @@ let sourceRemoteUrl = exec("git config remote.origin.url", { silent: true })
 // );
 // console.log("gst");
 
+
+//start
+
 exec(
   `
-zip -qr dist.zip ./req.js  
-`,
-  { silent: true }
-);
-console.log("zip dist done");
-
-cd(`../${destProjectName}/`);
+zip -qr dist.zip ./dist
+`, {
+    silent: true
+  }
+)
+console.log('zip dist done')
+//
+cd(`${commonPath}${destProjectName}/`)
 exec(
   `
 git reset --hard
 git checkout -b dev
 git checkout dev
-`,
-  { silent: true }
-);
-console.log("gco dev done");
+`, {
+    silent: true
+  }
+)
+console.log('gco dev done')
+console.log(process.cwd())
+//
+cp(
+  `${commonPath}${sourceProjectName}/dist.zip`,
+  `${commonPath}${destProjectName}/${destDistPath}`
+)
+console.log('cp dist done')
 
-cp(`../${sourceProjectName}/dist.zip`, `./`);
-console.log("cp dist done");
+rm('-rf', `${commonPath}${sourceProjectName}/dist.zip`)
+console.log('del sourceProjectName dist.zip done')
 
-let destRemoteUrl = exec("git config remote.origin.url", { silent: true })
-  .stdout;
-
-// 解压到当前目录
 exec(
   `
-unzip dist.zip
-`,
-  { silent: true }
-);
-console.log("unzip dist done");
+unzip -fo ${commonPath}${destProjectName}/${destDistPath}dist.zip
+`, {
+    silent: true
+  }
+)
+console.log('unzip dist done')
 
-rm("-rf", "dist.zip");
-console.log("del dist.zip done");
+rm('-rf', `${commonPath}${destProjectName}/${destDistPath}dist.zip`)
+console.log('del destProjectName dist.zip done')
 
+//
 exec(
   `
 git status
 git add -A
-`,
-  { silent: true }
+`, {
+    silent: true
+  }
 );
 // git commit -m 'update test code from ${who}'
 // git pull
